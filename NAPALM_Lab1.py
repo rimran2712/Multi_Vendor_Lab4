ced81d32341095f22a3ad51d791bfa20d3867d0f
Imran <rimran84@yahoo.com>
2024-09-22 12:16:54 -0500

    update inventory

--- NAPALM_Lab1.py
+++ NAPALM_Lab1.py
@@ -1,8 +1,15 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
+import os
+import ipdb
+from rich import print
 
-nr = InitNornir (config_file="/home/imran/Documents/Automation/Nornir/Runbooks/Multi_Vendor_Lab4/Inventory/config.yaml")
+nr = InitNornir (config_file="/home/imran/Documents/Automation/Nornir/Runbooks_Repositories/Multi_Vendor_Lab4/Inventory/config.yaml")
+
+# Clearing the Screen
+os.system('clear')
+
 
 def get_device_info (task):
     task.run (task=napalm_get, getters=["get_facts",
